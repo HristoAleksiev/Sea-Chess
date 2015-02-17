63e9072f7a46905baccf2cc12d467ca605016e1f
HristoAleksiev <kadoll555@gmail.com>
2015-02-17 23:11:41 +0200

    refactor (unsignificant)

--- SeaChess/SeaChess.cs
+++ SeaChess/SeaChess.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SeaChess
+﻿namespace SeaChess
 {
+    using System;
+
     public class SeaChess
     {
         public static void Main()
